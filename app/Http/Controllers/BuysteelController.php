c49b67b789d470e14cede684f8a89abd6b544560
Jasonnaja <102570734+Jasonnaja@users.noreply.github.com>
2022-07-25 13:31:22 +0700

    Create BuysteelController.php
    -update Buysteelcontroller

---
+++ app/Http/Controllers/BuysteelController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bank;
+use Illuminate\Http\Request;
+use Softon\SweetAlert\Facades\SWAL;
+use App\Branch;
+use App\Accounttype;
+use App\Http\Requests\BankRequest;
+
+
+class BuysteelController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+}
